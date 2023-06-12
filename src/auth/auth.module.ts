6624252067736d08a4078e9d40e058d1bfd11f74
kenkwanatgai <kjjkjjzyayufqza@yahoo.com>
2023-06-12 22:17:28 +0800

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { UsersModule } from '../users/users.module'
 import { JwtModule } from '@nestjs/jwt'
@@ -7,7 +7,7 @@ import { APP_GUARD } from '@nestjs/core'
 import { StaffUserModule } from 'src/staff-user/staff-user.module'
 
 @Module({
-  imports: [UsersModule,StaffUserModule],
+  imports: [forwardRef(() => UsersModule), forwardRef(() => StaffUserModule)],
   providers: [AuthService],
   controllers: [AuthController],
   exports: [AuthService],
